c8fd146200010c73e21896c5dad78d6a2afe6bdf
naimkim <naimk5451@gmail.com>
2025-04-21 21:48:59 +0900

    init

---
+++ src/main/java/hgu/tayo_be/TayoBeApplication.java
@@ -0,0 +1,13 @@
+package hgu.tayo_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TayoBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TayoBeApplication.class, args);
+    }
+
+}
